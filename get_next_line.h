9d3a8e3657b7cc1dad5679c5da9ff2720f38f208
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-05-11 11:41:08 +0200

    Create all needed files with required 42 header

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsordo-o <tsordo-o@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/11 11:37:23 by tsordo-o          #+#    #+#             */
+/*   Updated: 2026/05/11 11:39:51 by tsordo-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GETNEXTLINE_H
+# define GETNEXTLINE_H
+
+//Libraries
+#include <unistd.h>
+
+//Prototypes
+char *get_next_line(int fd);
+
+#endif
